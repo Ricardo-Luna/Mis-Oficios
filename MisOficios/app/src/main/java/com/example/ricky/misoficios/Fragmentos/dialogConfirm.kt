94c26128720f9f33541441b35a4cc86c18328586
Ricardo Luna <rickylunat@hotmail.com>
2019-09-24 12:04:07 -0500

    Ya muestra la ventana para confirmar la firma

---
+++ MisOficios/app/src/main/java/com/example/ricky/misoficios/Fragmentos/dialogConfirm.kt
@@ -0,0 +1,20 @@
+package com.example.ricky.misoficios.Fragmentos
+
+import android.app.Dialog
+import android.content.Context
+import android.databinding.DataBindingUtil.setContentView
+import android.os.Bundle
+import android.view.Window
+import com.example.ricky.misoficios.R
+
+class dialogConfirm(context: Context):Dialog(context) {
+    init {
+        setCancelable(false)
+
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        setContentView(R.layout.dialog_confirm)
+    }
+}
--- MisOficios/app/src/main/java/com/example/ricky/misoficios/Fragmentos/mostrarDocumento.kt
+++ MisOficios/app/src/main/java/com/example/ricky/misoficios/Fragmentos/mostrarDocumento.kt
@@ -2,6 +2,7 @@ package com.example.ricky.misoficios.Fragmentos
 
 import android.app.Activity
 import android.app.Dialog
+import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
@@ -14,23 +15,15 @@ import kotlinx.android.synthetic.main.activity_mostrar_documento.*
 import java.io.File
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog
+import android.view.LayoutInflater
+import android.view.Window
+import android.widget.Button
+import android.widget.TextView
 import java.io.IOException
 import java.io.OutputStreamWriter
 import android.widget.Toast
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+import kotlinx.android.synthetic.main.dialog_confirm.view.*
+import java.util.*
 
 
 class mostrarDocumento : AppCompatActivity() {
@@ -43,58 +36,46 @@ class mostrarDocumento : AppCompatActivity() {
         wb.getSettings().setDefaultZoom(WebSettings.ZoomDensity.FAR)
         wb.getSettings().setBuiltInZoomControls(true)
 
+
         val fab: FloatingActionButton = findViewById(R.id.fabFirmar)
         fab.setOnClickListener { view ->
             val bundle = Bundle()
             onCreateDialog(R.layout.dialog_confirm)
-
+            //onCreateDialog(R.layout.dialog_confirm, Bundle())
             val dl = AlertDialog.Builder(this)
-            dl.setTitle("Chinga tu madre Ed Maverick")
-                dl.setMessage("Seguro que quieres mandar a chingar a su madre a Ed Maverick?")
-            dl.setPositiveButton("Que chingue a su puta madre",
-                DialogInterface.OnClickListener { dialogo1, id -> aceptar() })
-            dl.setNegativeButton("Que chingue a su puta madre",
-                DialogInterface.OnClickListener { dialogo1, id -> aceptar() })
-            dl.show()
-
-            //
-            //     Snackbar.make(view, "Presione de nuevo para firmar documento", Snackbar.LENGTH_LONG)
-            //         .setAction("Action", null).show()
-            //     wb.loadUrl("https://www.amazon.com.mx/")
-            // }
-            val faback: FloatingActionButton = findViewById(R.id.floatingActionButton)
-            nuevoDoc()
-            faback.setOnClickListener { view ->
-                Snackbar.make(view, "Documento creado", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-                //  val intent = Intent(baseContext, MainActivity::class.java)
-                //  startActivity(intent)
-                wb.loadUrl("file:///android_asset/ss.html")
-
-            }
+            ShowDialog()
         }
+
+
+        val faback: FloatingActionButton = findViewById(R.id.floatingActionButton)
+        //  nuevoDoc()
+        faback.setOnClickListener { view ->
+            Snackbar.make(view, "Documento creado", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+
+            //  val intent = Intent(baseContext, MainActivity::class.java)
+            //  startActivity(intent)
+            //  wb.loadUrl("file:///android_asset/ss.html")
+
+
+        }
+
     }
 
-    fun nuevoDoc() {
-        try {
-            val archivo = OutputStreamWriter(openFileOutput("notas.txt", Activity.MODE_PRIVATE))
-            archivo.write("Salinas mató a colosio we obvio")
-            archivo.flush()
-            archivo.close()
-        } catch (e: IOException) {
-        }
-    }
-    fun aceptar() {
-        val t = Toast.makeText(this, "Gracias por mandar a chingar a su madre a Ed Maverick", Toast.LENGTH_SHORT)
-        t.show()
-    }
-    fun cancelar() {
-        finish()
-    }
+    //  fun nuevoDoc() {
+    //      try {
+    //          val archivo = OutputStreamWriter(openFileOutput("notas.txt", Activity.MODE_PRIVATE))
+    //          archivo.write("Salinas mató a colosio we obvio")
+    //          archivo.flush()
+    //          archivo.close()
+    //      } catch (e: IOException) {
+    //      }
+    //  }
+
     override fun onCreateDialog(id: Int, args: Bundle?): Dialog? {
         return mostrarDocumento().let {
-            val builder = AlertDialog.Builder(it)
-            val inflater = mostrarDocumento().layoutInflater;
+            val builder = AlertDialog.Builder(baseContext)
+            val inflater = dialogConfirm(baseContext).layoutInflater;
 
             builder.setView(inflater.inflate(R.layout.dialog_confirm, null))
                 // Add action buttons
@@ -110,7 +91,68 @@ class mostrarDocumento : AppCompatActivity() {
             builder.create()
         } ?: throw IllegalStateException("Activity cannot be null")
     }
+
+
+    private fun ShowDialog(){
+        val mDialogView = LayoutInflater.from(this).inflate(R.layout.dialog_confirm, null)
+        val mBuilder= AlertDialog.Builder(this)
+            .setView(mDialogView)
+            //.setTitle("Firmar")
+        val mAlertDialog = mBuilder.show()
+        mDialogView.Firmar.setOnClickListener()
+        {
+            mAlertDialog.dismiss()
+        }
+        val usuario = mDialogView.findViewById<TextView>(R.id.username).toString()
+        val password = mDialogView.findViewById<TextView>(R.id.password).toString()
+        val firmar = mDialogView.findViewById<Button>(R.id.Firmar).toString()
+
+    }
+
+
+    private fun showDialogDEP(title: String) {
+        val dialog = Dialog(dialogConfirm(baseContext).context)
+        //val dialog : Dialog = AlertDialog.Builder(baseContext)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(false)
+        dialog.setContentView(R.layout.dialog_confirm)
+        val usuario = dialog.findViewById<TextView>(R.id.username)
+        val password = dialog.findViewById<TextView>(R.id.password)
+        val firmar = dialog.findViewById<Button>(R.id.Firmar)
+        firmar.setOnClickListener {
+            dialog.dismiss()
+        }
+        dialog.show()
+    }
 }
+
+
+//
+//     Snackbar.make(view, "Presione de nuevo para firmar documento", Snackbar.LENGTH_LONG)
+//         .setAction("Action", null).show()
+//     wb.loadUrl("https://www.amazon.com.mx/")
+// }
+
+//Para crear un dialog
+
+//dl.setTitle("Chinga tu madre Ed Maverick")
+//dl.setMessage("Seguro que quieres mandar a chingar a su madre a Ed Maverick?")
+//dl.setPositiveButton("Que chingue a su puta madre",
+//DialogInterface.OnClickListener { dialogo1, id -> aceptar() })
+//dl.setNegativeButton("Que chingue a su puta madre",
+//DialogInterface.OnClickListener { dialogo1, id -> aceptar() })
+//dl.show()
+
+
+//fun aceptar() {
+//    val t = Toast.makeText(this, "Gracias por mandar a chingar a su madre a Ed Maverick", Toast.LENGTH_SHORT)
+//    t.show()
+//}
+//fun cancelar() {
+//    finish()
+//}
+
+
 // override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
 //     return mostrarDocumento().let {
 //         val builder = AlertDialog.Builder(it)
