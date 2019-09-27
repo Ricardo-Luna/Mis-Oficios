f0b63f9b41e691d15a7ea629c9a29b3e12dc570b
Ricardo Luna <rickylunat@hotmail.com>
2019-09-27 14:53:32 -0500

    Animaciones pendientes

--- MisOficios/app/src/main/java/com/example/ricky/misoficios/splash.kt
+++ MisOficios/app/src/main/java/com/example/ricky/misoficios/splash.kt
@@ -1,20 +1,45 @@
 package com.example.ricky.misoficios
 
+import android.animation.Animator
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
 import android.content.Intent
+import android.media.Image
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.Snackbar
+import android.view.View
+import android.view.animation.AnimationUtils
 import android.widget.ImageView
+import android.widget.TextView
+import com.example.ricky.misoficios.Modelos.oficios
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class splash : AppCompatActivity() {
     val DURACION = 1000;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        val im1 :ImageView = findViewById(R.id.imageView4)
+        val text : TextView=findViewById(R.id.oficiosText)
         val im : ImageView = findViewById(R.id.imageView5)
+
+        im1.setOnClickListener {
+        text.visibility = View.GONE
+            AnimationUtils.loadAnimation(this, R.anim.animacion_escalacion).also {
+                    hyperspaceJumpAnimation ->
+              //  findViewById<TextView>(R.id.oficiosText).startAnimation(hyperspaceJumpAnimation)
+                findViewById<ImageView>(R.id.imageView4).startAnimation(hyperspaceJumpAnimation)
+            }
+
+
+        }
+
+
         im.setOnClickListener {
             val intent = Intent(baseContext, login::class.java)
             startActivity(intent)
+
         }
         val background = object : Thread() {
             override fun run() {
@@ -33,5 +58,6 @@ class splash : AppCompatActivity() {
         }
         background.start()
     }
+
 
 }
